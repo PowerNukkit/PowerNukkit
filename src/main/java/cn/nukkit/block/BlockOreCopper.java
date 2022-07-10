f0c32127fbc66bdc4f6e7e0b87ccedb056138c0c
GoodLucky777 <good777good777good777@gmail.com>
2022-07-10 20:58:58 +0900

    Fix copper ore drops

--- src/main/java/cn/nukkit/block/BlockOreCopper.java
+++ src/main/java/cn/nukkit/block/BlockOreCopper.java
@@ -3,11 +3,10 @@ package cn.nukkit.block;
 import cn.nukkit.api.PowerNukkitOnly;
 import cn.nukkit.api.Since;
 import cn.nukkit.item.Item;
-import cn.nukkit.item.ItemRawCopper;
+import cn.nukkit.item.ItemID;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.item.enchantment.Enchantment;
-
-import java.util.concurrent.ThreadLocalRandom;
+import cn.nukkit.math.NukkitRandom;
 
 /**
  * @author GoodLucky777
@@ -64,21 +63,40 @@ public class BlockOreCopper extends BlockSolid {
     @Since("FUTURE")
     @Override
     public Item[] getDrops(Item item) {
-        if (item.isPickaxe() && item.getTier() >= getToolTier()) {
-            int count = 1;
-            Enchantment fortune = item.getEnchantment(Enchantment.ID_FORTUNE_DIGGING);
-            if (fortune != null && fortune.getLevel() >= 1) {
-                int i = ThreadLocalRandom.current().nextInt(fortune.getLevel() + 2) - 1;
-                if (i < 0) {
-                    i = 0;
-                }
-
-                count = i + 1;
-            }
-
-            return new Item[]{ new ItemRawCopper(0, count) };
-        } else {
+        if (!item.isPickaxe() || item.getTier() < this.getToolTier()) {
             return Item.EMPTY_ARRAY;
         }
+
+        Enchantment enchantment = item.getEnchantment(Enchantment.ID_FORTUNE_DIGGING);
+        int fortune = 0;
+        if (enchantment != null) {
+            fortune = enchantment.getLevel();
+        }
+
+        NukkitRandom nukkitRandom = new NukkitRandom();
+        int count = nukkitRandom.nextRange(2, 5);
+        switch (fortune) {
+            case 0:
+                // Does nothing
+                break;
+            case 1:
+                if (nukkitRandom.nextRange(0, 2) == 0) {
+                    count *= 2;
+                }
+                break;
+            case 2:
+                if (nukkitRandom.nextRange(0, 1) == 0) {
+                    count *= nukkitRandom.nextRange(2, 3);
+                }
+                break;
+            default:
+            case 3:
+                if (nukkitRandom.nextRange(0, 4) < 3) {
+                    count *= nukkitRandom.nextRange(2, 4);
+                }
+                break;
+        }
+
+        return new Item[]{ Item.get(ItemID.RAW_COPPER, 0, count) };
     }
 }
